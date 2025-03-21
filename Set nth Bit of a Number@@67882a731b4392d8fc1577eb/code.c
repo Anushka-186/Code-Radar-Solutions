9054d09a02e8ea2420c0791428e097eba1c829db
Anushka <anushkaaggarwal186@gmail.com>
2025-03-21 09:58:28 +0530

    Solution to Set nth Bit of a Number

---
+++ Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+unsigned int setNthBit(unsigned int num, int n) {
+    // Create a mask by left-shifting 1 to the nth position
+    unsigned int mask = 1 << n;
+    
+    // Perform the bitwise OR operation to set the nth bit to 1
+    return num | mask;
+}
+
+int main() {
+    unsigned int num;
+    int n;
+
+    // Input the number and the bit position to set
+    printf("Enter a number: ");
+    scanf("%u", &num);
+    printf("Enter the bit position (n): ");
+    scanf("%d", &n);
+
+    // Call the function to set the nth bit
+    unsigned i
