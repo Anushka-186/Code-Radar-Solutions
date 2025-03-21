4872b9d3c27d11fd15baf9842e449dbc716458e3
Anushka <anushkaaggarwal186@gmail.com>
2025-03-21 10:01:58 +0530

    Solution to Flip Bits of a Binary Number

--- Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
+++ Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// Function to print binary representation of a number
-void printBinary(unsigned int n) {
-    for (int i = sizeof(n) * 8 - 1; i >= 0; i--) {
+// Function to print the binary representation of a number
+void printBinary(unsigned int num, int bitSize) {
+    for (int i = bitSize - 1; i >= 0; i--) {
         // Check if the ith bit is 1 or 0
-        if ((n >> i) & 1) {
+        if ((num >> i) & 1) {
             printf("1");
         } else {
             printf("0");
@@ -13,26 +13,32 @@ void printBinary(unsigned int n) {
     printf("\n");
 }
 
-// Function to flip all bits using bitwise NOT
-unsigned int flipBits(unsigned int n) {
-    return ~n;  // Flip all bits using bitwise NOT operator
-}
-
 int main() {
     unsigned int num;
+    int bitSize;
 
-    // Get the input from the user
+    // Input the decimal number and the bit size (e.g., 8, 16, 32)
     printf("Enter a number: ");
     scanf("%u", &num);
-
-    unsigned int flipped = flipBits(num);
-
-    // Output the original and flipped binary values
-    printf("Original number in binary: ");
-    printBinary(num);
     
+    printf("Enter the bit size (e.g., 8, 16, 32): ");
+    scanf("%d", &bitSize);
+
+    // Validate the bit size
+    if (bitSize != 8 && bitSize != 16 && bitSize != 32) {
+        printf("Invalid bit size. Supported sizes are 8, 16, and 32.\n");
+        return 1;
+    }
+
+    // Perform the bitwise NOT operation (flip all bits)
+    unsigned int flippedNum = ~num;
+
+    // Output the original and flipped numbers in binary
+    printf("Original number in binary: ");
+    printBinary(num, bitSize);
+
     printf("Flipped number in binary: ");
-    printBinary(flipped);
+    printBinary(flippedNum, bitSize);
 
     return 0;
 }
